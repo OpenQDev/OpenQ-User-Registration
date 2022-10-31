105a7c99db16db8ababf401c75b33a788d225858
FlacoJones <andrew@openq.dev>
2022-10-31 18:58:21 -0500

    removes unused imports

--- index.js
+++ index.js
@@ -8,24 +8,19 @@ const PRODUCTION_AUTOTASK_ID = '1224e6b1-20f6-4f55-96b1-f9cf0683ebc8';
 // Autotask Entrypoint - constructs signer and contract using Relay
 exports.handler = async (event) => {
 	let OPENQ_PROXY_ADDRESS;
-	let CLAIM_MANAGER_PROXY_ADDRESS;
-	console.log('event.autotaskId', event.autotaskId);
+
 	switch (event.autotaskId) {
 		case STAGING_AUTOTASK_ID:
 			OPENQ_PROXY_ADDRESS = event.secrets.OPENQ_PROXY_ADDRESS_STAGING;
-			CLAIM_MANAGER_PROXY_ADDRESS = event.secrets.CLAIM_MANAGER_PROXY_ADDRESS_STAGING;
 			break;
 		case PRODUCTION_AUTOTASK_ID:
 			OPENQ_PROXY_ADDRESS = event.secrets.OPENQ_PROXY_ADDRESS_PRODUCTION;
-			CLAIM_MANAGER_PROXY_ADDRESS = event.secrets.CLAIM_MANAGER_PROXY_ADDRESS_PRODUCTION;
 			break;
 		default:
 			OPENQ_PROXY_ADDRESS = event.secrets.OPENQ_PROXY_ADDRESS;
-			CLAIM_MANAGER_PROXY_ADDRESS = event.secrets.CLAIM_MANAGER_PROXY_ADDRESS;
 	}
 
 	console.log('OPENQ PROXY ADDRESS', OPENQ_PROXY_ADDRESS);
-	console.log('CLAIM_MANAGER_PROXY_ADDRESS', CLAIM_MANAGER_PROXY_ADDRESS);
 
 	const { DefenderRelayProvider, DefenderRelaySigner } = require('defender-relay-client/lib/ethers');
 	const { ethers } = require('ethers');
@@ -36,11 +31,10 @@ exports.handler = async (event) => {
 
 	// Prepare OpenQ Contract for call
 	const openQ = new ethers.Contract(OPENQ_PROXY_ADDRESS, OPENQ_ABI, signer);
-	const claimManager = new ethers.Contract(CLAIM_MANAGER_PROXY_ADDRESS, CLAIM_MANAGER_ABI, signer);
 
 	// We then run the main logic in the main function
 	try {
-		const result = await main(event, openQ, claimManager);
+		const result = await main(event, openQ);
 		return result;
 	} catch (error) {
 		return error;
